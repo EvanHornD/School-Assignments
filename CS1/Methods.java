a8d2b6b03ed8606349f8520cbc3d29caa6d2b958
Evan Horn <ehorn01@yahoo.com>
2024-03-22 08:27:08 -0600

    All of my CS files on my main PC

---
+++ CS1/Methods.java
@@ -0,0 +1,78 @@
+public class Methods {
+    public static int robotAntennas(int n){
+        int antennas = 0;
+        for(int i=0;i<n;i++){antennas+=3;}
+        return(antennas);
+    }
+    public static int robotAntennasEO(int n){
+        int antennas = 0;
+        for(int i=1;i<=n;i++){antennas+=4-(i%2);}
+        return(antennas);
+    }
+    public static int sumDigits(int n){
+        int total = 0;
+        for(double i=1;i<=String.valueOf(n).length();i++){total+=((n%Math.pow(10.0,i))-total)/Math.pow(10.0,i-1);}
+        return(total);
+    }
+    public static int count7(int n){
+        int sevens = 0;
+        for(int i=0;i<String.valueOf(n).length();i++){if(String.valueOf(n).charAt(i)=='7'){sevens+=1;};}
+        return(sevens);
+    }
+    public static String changeVowels(String s){
+        String vowels = "";
+        for(int i=0;i<s.length();i++){
+            switch(s.charAt(i)){
+                case'a': vowels+='z';break;
+                case'e': vowels+='z';break;
+                case'i': vowels+='z';break;
+                case'o': vowels+='z';break;
+                case'u': vowels+='z';break;
+                default: vowels+=s.charAt(i);
+            }
+            if(s.charAt(i)=='7'){};}
+        return(vowels);
+    }
+    public static void printOrderedNums(int a, int b, int c){
+        if(a>b){
+            if(a>c){
+                if(b>c){
+                    System.out.println(a+" "+b+" "+c);
+                } else {
+                    System.out.println(a+" "+c+" "+b);
+                }
+            } else {
+                System.out.println(c+" "+a+" "+b);
+            }
+        } else if(b>c){
+            if(c>a){
+                System.out.println(b+" "+c+" "+a);
+            } else {
+                System.out.println(b+" "+a+" "+c);
+            }
+        } else {
+            System.out.println(c+" "+b+" "+a);
+        }
+
+    }
+    public static void printAverage(int a, int b, int c, int d, int e){
+        System.out.println((a+b+c+d+e)/5.0);
+    }
+    public static void printCharNum(String s,char c){
+        int charNum = 0;
+        for(int i=0;i<s.length();i++){if(s.charAt(i)==c){charNum+=1;}}
+        System.out.println(charNum);
+    }
+    public static void printIsValidPassword(String s){
+        System.out.println(s.length()>=8);
+    }
+    public static void printIsMultiple(int n,int m){
+        System.out.println(n%m==0);
+    }
+    public static void printComputeArea(int n){
+        System.out.println((n*n)*3.14);
+    }
+    public static void main(String[] args) {
+        printComputeArea(10);
+    }
+}
---
+++ Integration solver/Integration.java
@@ -0,0 +1,30 @@
+public class Integration {
+
+    static double function1(double x) {
+        return Math.sin(x)+1;
+    }
+
+    static double function2(double x) {
+        return Math.sin(x);
+    }
+
+    static double calculateIntegral(double start,double end,int n){
+        double area = 0;
+        for(int i = 0; i < n; i++){
+            double rectangleWidth = (end-start)/n;
+            double rectangleX = start + (i * rectangleWidth);
+            double rectangleHeight = function1(rectangleX) - function2(rectangleX);
+            area = area + rectangleWidth * rectangleHeight;;
+        }
+        return(area);
+    }
+
+    public static void main(String[] args)
+    {
+        double start = 0;
+        double end = Math.PI;
+        int numberOfIterations = 1000000;
+        System.out.println(calculateIntegral(start,end,numberOfIterations));
+
+    }
+}
