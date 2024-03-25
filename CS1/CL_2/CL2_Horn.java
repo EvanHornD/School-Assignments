3df32de699ce0e9df6d5ffea5c6c9a7409a06968
Evan Horn <ehorn01@yahoo.com>
2024-03-25 10:15:47 -0600

    commented all of the methods

--- CS1/CL_2/CL2_Horn.java
+++ CS1/CL_2/CL2_Horn.java
@@ -16,7 +16,7 @@ public class CL2_Horn {
     static int[][] playerMovement = {{-1,0},{0,-1},{1,0},{0,1}};
     static Scanner userInput = new Scanner(System.in);
 
-    // The purpose of this method is to
+    // The purpose of this method is to get all of the maze files and add them to an array without the user having to type the name of the file
     public static File[] loadTxtFiles(String FilePath){
         //This line takes the file path and returns the parent directory
         String directoryName = new File(FilePath).getParent();
@@ -47,7 +47,8 @@ public class CL2_Horn {
         return(MazeFiles);
     }
 
-    public static char[][][] readTxtFiles(File[] txtFiles){
+    //reads all of the text files that were created in the loadtext files method and stores them into 1 large 3 dimensional array of mazes
+    public static char[][][] createSquareMazes(File[] txtFiles){
         char[][][] mazes = new char[txtFiles.length][][];
         for(int i = 0; i < txtFiles.length; i++){
             try {
@@ -74,8 +75,9 @@ public class CL2_Horn {
         return(mazes);
     }
 
-    public static void loadFile(int mazeNum){
-        findSpecialChars(mazeNum);
+    //loades a maze into 2 separate arrays, one that will be updated and displayed and another that will be used to store the original maze without any changes made to it
+    public static void loadSquareMaze(int mazeNum){
+        findStartAndEndPostion(mazeNum);
         loadedMaze = mazes[mazeNum];
         mazeDimenstions[0] = loadedMaze.length;
         mazeDimenstions[1] = loadedMaze[0].length;
@@ -83,7 +85,8 @@ public class CL2_Horn {
         
     }
 
-    public static void findSpecialChars(int mazeNum){
+    //loops through all of the characters in the maze template and stores the char and its coordinates into separate arrays for later use
+    public static void findStartAndEndPostion(int mazeNum){
         int numOfChars = 0;
         for(int i=0;i<mazes[mazeNum].length;i++){for(int ii=0;ii<mazes[mazeNum][i].length;ii++){
             switch (mazes[mazeNum][i][ii]) {
@@ -103,17 +106,20 @@ public class CL2_Horn {
             }}}
     }
 
+    //sets the characters starting position in the maze based on the values gained from the find start and end positions method
     public static void setPlayerCoords() {
         for(int i=0;i<specialChars.length;i++){
             if(specialChars[i]=='S'){playerCoords=specialCharCoords[i];break;}}
     }
 
+    //when given a special character it will give you the coordinates that were stored instead of having to traverse the entire mazes array
     public static int[] getCoords(char C){
         for(int i=0;i<specialChars.length;i++){
             if(C==specialChars[i]){return(specialCharCoords[i]);}}
         return(specialCharCoords[0]);
     }
 
+    //will detect the users input and will return a corresponding value
     public static int getInput(){
         switch (userInput.nextLine().toLowerCase()) {
             case "w": return(0);
@@ -125,25 +131,29 @@ public class CL2_Horn {
         }
     }
 
+    //when given a coordinate it will check if it is a wall or if it is out of bounds
     public static boolean checkPos(int[] pos){
         if((pos[0]<mazeDimenstions[0])&&(pos[0]>=0)&&(pos[1]<mazeDimenstions[1])&&(pos[1]>=0)){
             switch (loadedMaze[pos[0]][pos[1]]) {
-                case '#':return(false);
+                case '#':System.out.println("you cant move through walls"); return(false);
                 default:return(true);
             }}
         return(false);
     }
 
+    //takes in an integer that is given by the getInput method and will update the players coordinates 
     public static void movePlayer(int input){
         int[] newPos = new int[2];
         if(input>=0&&input<4){
             for(int i=0;i<playerCoords.length;i++){
+                // the player movement array stores the changes that a specific input would have on the player {{-1,0},{0,-1},{1,0},{0,1}}
                 newPos[i] = playerCoords[i]+playerMovement[input][i];}}
         if(checkPos(newPos)){
             playerCoords[0]=newPos[0];
             playerCoords[1]=newPos[1];}
     }
 
+    //updates the menu cursor based on the input the player gives
     public static int moveCursor(int cursor,int input){
         switch (input) {
             case 0:if(cursor-1>=0){return(cursor-1);}return(cursor);
@@ -151,6 +161,7 @@ public class CL2_Horn {
             default:return(cursor);}
     }
 
+    //updates the loaded maze with players coordinates applied to the template.  this has to be done as a for loop to avoid the 2 arrays being assigned to the same memory location
     public static char[][] updateMaze(){
         char[][] updatedMaze = new char[loadedMazeTemplate.length][loadedMazeTemplate[0].length];
         for(int i=0;i<loadedMazeTemplate.length;i++){
@@ -160,11 +171,13 @@ public class CL2_Horn {
         return(updatedMaze);
     }
     
+    // this method would have cleared the console right before printing out the maze but I would probably be deducted points for having it because I dont know how it works
     // public static void clearconsole(){
     //     try { new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();}
     //     catch (Exception e) {}
     // }
 
+    //loops throught all of the maze names and prints them in order adding in an arrow if the index of the maze is the same as the cursor
     public static void printMenu(int cursor){
         //clearconsole();
         for(int i=0;i<fileNames.length;i++){
@@ -172,9 +185,11 @@ public class CL2_Horn {
             else{System.out.println(fileNames[i].getName().replace(".txt",""));}}
         if(cursor==5){System.out.println("-->Exit");}
         else{System.out.println("Exit");}
+        System.out.println("Use W and S to traverse the files\nPress E to choose a maze");
     }
 
-    public static void printmaze(){
+    //prints the loaded maze
+    public static void displayMaze(){
         //clearconsole();
         String mazeLine = "";
         for(int i=0;i<loadedMaze.length;i++){
@@ -185,7 +200,7 @@ public class CL2_Horn {
 
     public static void main(String[] args) {
         fileNames = loadTxtFiles("C:\\Users\\Evan Horn\\GitRepositories\\School-Assignments\\CS1\\CL_2\\CL2_Horn.java");
-        mazes = readTxtFiles(fileNames);
+        mazes = createSquareMazes(fileNames);
         int menuCursor = 0;
         while(gameState!="Exit"){
             switch (gameState) {
@@ -200,19 +215,17 @@ public class CL2_Horn {
                             else{gameState="runningMaze";}}
                     }break;
                 case "runningMaze":
-                    loadFile(menuCursor);
+                    loadSquareMaze(menuCursor);
                     setPlayerCoords();
                     loadedMaze=updateMaze();
-                    printmaze();
+                    displayMaze();
                     while (gameState=="runningMaze") {
                         int input = getInput();
                         if(input == 4){gameState="mazeSelect";break;}
                         movePlayer(input);
                         loadedMaze=updateMaze();
-                        printmaze();
-                        if(Arrays.equals(playerCoords,getCoords('F'))){gameState="mazeSelect";break;}
-                        // System.out.println(Arrays.toString(playerCoords));
-                        // System.out.println(Arrays.toString(getCoords('F')));
+                        displayMaze();
+                        if(Arrays.equals(playerCoords,getCoords('F'))){gameState="mazeSelect";System.out.println("Congrats you beat: " + fileNames[menuCursor].getName().replace(".txt",""));break;}
                     } break;
             }}
     }
