21dcfd56eaf053fb8c43aeb8bd60eb1e42cfb98a
Evan Horn <ehorn01@yahoo.com>
2024-04-07 02:06:45 -0600

    testing screen dimensions

--- testCode.java
+++ testCode.java
@@ -1,32 +1,29 @@
 import java.io.File;
 import java.util.Arrays;
+import java.awt.*;
 
 public class testCode {
+    public static int[] getScreenDimensions(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int[] dimensions = {(int)screenSize.getWidth()/7,(int)screenSize.getHeight()/15};
+        return (dimensions);
+    }
+
     public static void clearConsole() {
         // Clearing the console by printing a newline character multiple times
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-    public static File[] loadFiles(){
-        // FilePath EX: C:\\Users\\User\\School-Assignments\\testCode.java
-        try{
-            String workingDirectory = System.getProperty("user.dir");
-            File directory = new File(workingDirectory);
-            File[] files = {};
-            if (directory.isDirectory()){
-                files = directory.listFiles();
-                // does some work
-            }
-            return files;
-        }
-        catch(Exception e){
-            System.out.println(e);
-            File[] invalidDirectory = {};
-            return(invalidDirectory);
-        }
-    }
+
     public static void main(String[] args) {
-        File[] files = loadFiles();
-        System.out.println(Arrays.toString(files));
+        String screenWidth ="";
+        int[] dimensions = getScreenDimensions();
+        for(int i=0; i<dimensions[0]-3;i++){
+            screenWidth+="#";
+        }
+        clearConsole();
+        for(int i=0; i<dimensions[1]-3;i++){
+            System.out.println(screenWidth);
+        }
     }
 }
