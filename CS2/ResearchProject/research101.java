2d900c2613044c59f20a103410a5448485377d4f
Evan Horn <ehorn01@yahoo.com>
2024-09-12 08:23:15 -0600

    xmlreader started

---
+++ CS2/ResearchProject/research101.java
@@ -0,0 +1,9 @@
+
+import java.io.IOException;
+
+public class research101 {
+    public static void main(String[] args) throws IOException {
+        xmlReader reader = new xmlReader("CS2\\ResearchProject\\cwec_v4.15.xml");
+        reader.printWeaknessNames();
+    }
+}
---
+++ CS2/ResearchProject/xmlReader.java
@@ -0,0 +1,59 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class xmlReader {
+    private RandomAccessFile xmlFile;
+    private String filePath;
+    private Map<Integer, Long> lineIndex;
+    private ArrayList<String[]> weaknesses;
+
+    public xmlReader(String filePath) throws IOException{
+        this.filePath = filePath;
+        this.xmlFile = new RandomAccessFile(filePath, "r");
+        this.lineIndex = new HashMap<>();
+        this.weaknesses = new ArrayList<String[]>();
+        indexFile();
+    }
+
+    private void indexFile() throws IOException {
+        long fileIndexPointer = 0;
+        int lineNumber = 0;
+        String line;
+
+        // Open a temporary BufferedReader to index the file
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while ((line = reader.readLine()) != null) {
+                if(line.contains("<Weakness ")){
+                    lineIndex.put(lineNumber, fileIndexPointer);
+                    String name = "";
+                    for(int i=0;i<line.length();i++){
+                        if(line.charAt(i)=='N'){
+                            i+=6;
+                            while(line.charAt(i)!='"'){
+                                name+=line.charAt(i);
+                                i++;
+                            }
+                        }
+                    }
+                    String[] n = {name,Long.toString(lineNumber)};
+                    weaknesses.add(n);
+                }
+                fileIndexPointer += line.getBytes().length + System.lineSeparator().getBytes().length;
+                lineNumber++;
+            }
+        }
+    }
+    
+    public void printWeaknessNames() {
+        for(int i=0; i<weaknesses.size();i++){
+            System.out.println(Arrays.toString(weaknesses.get(i))+" "+lineIndex.get(Integer.parseInt(weaknesses.get(i)[1])));
+        }
+    }
+
+    public void close() throws IOException {
+        xmlFile.close();
+    }
+}
