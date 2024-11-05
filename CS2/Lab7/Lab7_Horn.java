3453a0a56e9c843b6bfdd1a96e2ad2b7030ba0a4
Evan Horn <ehorn01@yahoo.com>
2024-11-04 18:17:57 -0700

    comments

--- CS2/Lab7/Lab7_Horn.java
+++ CS2/Lab7/Lab7_Horn.java
@@ -2,6 +2,13 @@ package Lab7;
 
 import javax.swing.SwingUtilities;
 
+// this file is for launching the project
+// if you want to see the sorting algorthims they are in the Lab7_SortingAlgorithms
+//
+// if the project is giving you an error when running the file then you need to open each of the separate class files individually for them to load properly
+//
+// to use earch of the different sorting algorithms you need to click on the header that you want to sort by, and change the sorting type by clicking on the button
+
 public class Lab7_Horn {
             public static void main(String[] args) {
             SwingUtilities.invokeLater(() -> {
